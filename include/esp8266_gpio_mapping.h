5a2b1c8f84d55cf1f570521312ab8cb8a0643681
bram <nogrend@msn.com>
2023-03-03 16:56:07 +0100

    add lib ds3231

---
+++ include/esp8266_gpio_mapping.h
@@ -0,0 +1,19 @@
+#define A0 17 // #define A0 0 // ADC0 ???
+#define D0 16
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+#define TXD0 1
+#define RXD0 3
+#define SCL 5
+#define SDA 4
+#define WAKE 16
+#define SCLK 14
+#define MISO 12
+#define MOSI 13
+#define CS 15
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,113 @@
 #include <Arduino.h>
+#include <LCD_4_5_Digits.h>
+#include <esp8266_gpio_mapping.h>
+// #include <DS3231.h>
+// #include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "../login.h"
 
-void setup() {
-  // put your setup code here, to run once:
+// 0x57
+// 0x68
+
+// prototype
+void setup_wifi(void);
+void callback(String topic, byte *message, uint32_t length);
+void reconnect(void);
+// void updateShiftRegister();
+
+// 21, 22, 23 // example
+// 21, 27, 12 // esp32
+//  8,   ,     // uno
+//  1,   ,      // esp8266
+
+// // for ESP32 Microcontrollers
+// uint8_t latchPin = 21;
+// uint8_t clockPin = 13;
+// uint8_t dataPin = 12;
+
+// for ESP8266 Microcontrollers
+// uint8_t latchPin = 5; // 1
+// uint8_t clockPin = 4; // 2
+// uint8_t dataPin = 2;  // 3
+
+uint8_t latchPin = D8; // 1
+uint8_t clockPin = D6; // 2
+uint8_t dataPin = D7;  // 3
+
+LCD_4_5_Digits lcd(latchPin, clockPin, dataPin);
+
+const char *ssid = SSID;
+const char *password = SSID_PASSWORD;
+const char *mqtt_server = MQTT_SERVER;
+
+WiFiClient phaseThreeClient;
+PubSubClient client(phaseThreeClient);
+
+void setup()
+{
+  // Serial.begin(9600);
+  Serial.println("let's start");
+  lcd.init();
+  lcd.set_value(10801);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect("phaseThreeClient");
+  // uint16_t light_sensor_value = analogRead(A0);
+  // lcd.set_value(light_sensor_value);
+  delay(500);
+}
+
+void setup_wifi(void)
+{
+  delay(10);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    // Serial.print(".");
+    delay(500);
+  }
+}
+
+void callback(String topic, byte *message, uint32_t length)
+{
+  String messageTemp;
+
+  for (uint32_t i = 0; i < length; i++)
+  {
+    messageTemp += (char)message[i];
+  }
+
+  if (topic == "energy_meter/data_6")
+  {
+    float powerMeterPhaseThreeFloat = messageTemp.toFloat();
+    uint16_t powerMeterPhaseThreeInt = uint16_t(powerMeterPhaseThreeFloat * 1000);
+    lcd.set_value(powerMeterPhaseThreeInt);
+  }
+}
+
+void reconnect(void)
+{
+  while (!client.connected())
+  {
+    if (client.connect("phaseThreeClient"))
+    {
+      client.subscribe("energy_meter/data_6");
+    }
+    else
+    {
+      // Serial.println("reconnect, moment please");
+      delay(5000);
+    }
+  }
 }
