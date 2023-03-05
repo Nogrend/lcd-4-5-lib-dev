afb765f364a9d348be0167910d10d3e0d1130bc1
bram <nogrend@msn.com>
2023-03-05 19:17:54 +0100

    test code

--- src/main.cpp
+++ src/main.cpp
@@ -34,23 +34,48 @@ LCD_4_5_Digits lcd(latchPin, clockPin, dataPin);
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("let's start");
+  // Serial.begin(9600);
+  // Serial.println("let's start");
   lcd.init();
   lcd.set_integer(6666);
 }
 
 void loop()
 {
+  lcd.set_float(0.1, 1);
+  delay(1000);
+  lcd.set_float(0.01, 2);
+  delay(1000);
+  lcd.set_float(0.001, 3);
+  delay(1000);
+  lcd.set_float(0.0001, 4);
+  delay(1000);
+
   for (float i = -1.0; i < 1.0; i += 0.1)
   {
-    lcd.set_float(i, 0);
+    lcd.set_float(i, 1);
     delay(1000);
+  }
+
+  for (float i = -1.00; i < 1.00; i += 0.01)
+  {
+    lcd.set_float(i, 2);
+    delay(100);
+  }
+  for (float i = -1.000; i < 1.000; i += 0.001)
+  {
+    lcd.set_float(i, 3);
+    delay(100);
   }
 
   for (float i = -110.0; i < 110.0; i += 0.1)
   {
-    lcd.set_float(i, 0);
+    lcd.set_float(i, 1);
+    delay(100);
+  }
+  for (float i = -110.0; i < 110.0; i += 0.01)
+  {
+    lcd.set_float(i, 2);
     delay(100);
   }
 }
